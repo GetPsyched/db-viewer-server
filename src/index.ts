79bf7b2c773dc1839bb5d2a51c4723323ce88dd5
GetPsyched <priyanshutr@gmail.com>
2022-11-23 17:56:08 +0530

    Implement data fetching
    - Add 'dsn' and 'query' as input parameters
    - Restrict query to only SELECT statements
    - Return the queried data as a response

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,39 @@
-import bodyParser from "body-parser";
-import express from "express";
-import pg from "pg";
-
-const pool = new pg.Pool();
+import bodyParser from 'body-parser';
+import { body, validationResult } from 'express-validator';
+import express from 'express';
+import pg from 'pg';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.api+json" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw({ type: 'application/vnd.api+json' }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! You've successfully connected to the database`);
-});
+app.post(
+  '/',
+  body('query').matches(/^SELECT .+$/i),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const dsn = req.body.dsn;
+    const query = req.body.query;
+
+    // Initialise and connect to the database
+    const client = new pg.Client({connectionString: dsn})
+    client.connect((err) => {
+      if (err) {
+        res.status(400).json({ errors: { err }})
+      }
+    })
+
+    const { rows } = await client.query(query);
+    res.send(rows);
+  },
+);
 
 app.listen(port, () => {
   console.log(`db-viewer-server listening at http://localhost:${port}`);
