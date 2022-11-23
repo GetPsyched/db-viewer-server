f7b7a0c6672b285bacad9b7c090e2581117ec70a
GetPsyched <priyanshutr@gmail.com>
2022-11-23 16:28:05 +0530

    Add sample code for the web framework
    - Connect to the PostgreSQL db
    - Listen to '/' and send a generic response
    - Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
+
+const pool = new pg.Pool();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: "application/vnd.api+json" }));
+app.use(bodyParser.text({ type: "text/html" }));
+
+app.get("/", async (req, res) => {
+  const { rows } = await pool.query("SELECT NOW()");
+  res.send(`Hello, World! You've successfully connected to the database`);
+});
+
+app.listen(port, () => {
+  console.log(`db-viewer-server listening at http://localhost:${port}`);
+});
