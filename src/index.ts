549803e8f767ebb58209f507ab856e497e1edefe
GetPsyched <priyanshutr@gmail.com>
2022-11-27 00:29:41 +0530

    Ditched bodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import bodyParser from 'body-parser';
 import { body, validationResult } from 'express-validator';
 import express from 'express';
 import pg from 'pg';
@@ -8,9 +7,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.raw({ type: 'application/vnd.api+json' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.raw({ type: 'application/json' }));
 
 app.post(
   '/',
