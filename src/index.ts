9f23ef1de19acf925b212f446d65c47cd80821df
GetPsyched <priyanshutr@gmail.com>
2022-11-28 16:04:05 +0530

    Run code formatter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { body, validationResult } from 'express-validator';
-import express from 'express';
-import pg from 'pg';
-import cors from 'cors';
+import { body, validationResult } from "express-validator";
+import express from "express";
+import pg from "pg";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,32 +9,28 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.raw({ type: 'application/json' }));
+app.use(express.raw({ type: "application/json" }));
 
-app.post(
-  '/',
-  body('query').matches(/^SELECT .+$/i),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+app.post("/", body("query").matches(/^SELECT .+$/i), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const dsn = req.body.dsn;
+  const query = req.body.query;
+
+  // Initialise and connect to the database
+  const client = new pg.Client({ connectionString: dsn });
+  client.connect((err) => {
+    if (err) {
+      res.status(400).json({ errors: { err } });
     }
+  });
 
-    const dsn = req.body.dsn;
-    const query = req.body.query;
-
-    // Initialise and connect to the database
-    const client = new pg.Client({connectionString: dsn})
-    client.connect((err) => {
-      if (err) {
-        res.status(400).json({ errors: { err }})
-      }
-    })
-
-    const { rows } = await client.query(query);
-    res.send(rows);
-  },
-);
+  const { rows } = await client.query(query);
+  res.send(rows);
+});
 
 app.listen(port, () => {
   console.log(`db-viewer-server listening at http://localhost:${port}`);
