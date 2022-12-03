619645c7e0017e09a864a030858109adea769a70
GetPsyched <priyanshutr@gmail.com>
2022-12-04 00:57:43 +0530

    Add GET for INFORMATION_SCHEMA

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,51 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.raw({ type: "application/json" }));
 
+type InformationSchema = {
+  database: string;
+  table: string;
+  column: string;
+};
+
+app.get("/schema", async (req, res) => {
+  // Initialise and connect to the database
+  const client = new pg.Client({ connectionString: req.body.dsn });
+  client.connect((err) => {
+    if (err) {
+      res.status(400).json({ errors: { err } });
+    }
+  });
+
+  const query =
+    `SELECT ` +
+    `table_catalog AS database,` +
+    `table_name AS table,` +
+    `column_name AS column ` +
+    `FROM ` +
+    `INFORMATION_SCHEMA.columns ` +
+    `WHERE ` +
+    `table_schema = 'public' ` +
+    `ORDER BY ` +
+    `ordinal_position`;
+  const rows: Array<InformationSchema> = (await client.query(query)).rows;
+
+  var nested_object: any = {};
+  rows.forEach((row: InformationSchema) => {
+    if (nested_object[row.database]) {
+      if (nested_object[row.database][row.table]) {
+        nested_object[row.database][row.table].push(row.column);
+      } else {
+        nested_object[row.database][row.table] = new Array(row.column);
+      }
+    } else {
+      nested_object[row.database] = {};
+      nested_object[row.database][row.table] = new Array(row.column);
+    }
+  });
+
+  res.send(nested_object);
+});
+
 app.post("/", body("query").matches(/^SELECT .+$/i), async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
